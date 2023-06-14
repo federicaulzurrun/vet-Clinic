36bba18b32eaccc5007610b4e3fa3078c3069ffe
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-06-14 12:52:32 -0300

    delete, create savepoint, update rollback

--- queries.sql
+++ queries.sql
@@ -30,3 +30,13 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+/* DELETE, CREATE a SAVEPOINT, UPDATE */
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT savepointone;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO savepointone;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
