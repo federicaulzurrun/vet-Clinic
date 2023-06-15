70f941f17a54ee587dbaf952e37d4fe87978ace0
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-06-15 18:36:21 -0300

    added visits, vets and specializations tables

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE animals (
   name VARCHAR(255) NOT NULL,
   date_of_birth DATE NOT NULL,
   escape_attempts INT NOT NULL,
-  neutered BOOLEAN NOT NULL,
+  neutered BOOLEAN aNOT NULL,
   weight_kg DECIMAL NOT NULL
 );
 
@@ -31,3 +31,38 @@ ALTER TABLE animals
   ADD COLUMN species_id INTEGER REFERENCES species(id),
   ADD COLUMN owner_id INTEGER REFERENCES owners(id),
   DROP COLUMN species;
+
+/* Added vets table */
+CREATE TABLE vets (
+    id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    date_of_graduation DATE
+);
+
+/* Added specializations table*/
+CREATE TABLE specializations (
+    vet_id INT,
+    species_id INT,
+    PRIMARY KEY (vet_id, species_id),
+    CONSTRAINT fk_vets
+      FOREIGN KEY (vet_id) REFERENCES vets(id)
+      ON DELETE CASCADE,
+    CONSTRAINT fk_species
+      FOREIGN KEY (species_id) REFERENCES species(id)
+      ON DELETE CASCADE
+);
+
+/* Added visits table*/
+CREATE TABLE visits (
+    id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    animal_id INT,
+    vet_id INT,
+    date_of_visit DATE,
+    CONSTRAINT fk_vets
+      FOREIGN KEY (vet_id) REFERENCES vets(id)
+      ON DELETE CASCADE,
+    CONSTRAINT fk_animals
+      FOREIGN KEY (animal_id) REFERENCES animals(id)
+      ON DELETE CASCADE
+);
